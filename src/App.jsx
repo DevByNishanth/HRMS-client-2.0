49e1685728350845f62b5dc890f4fa22a636d9f2
dharshan-m <dharshanmohan2704@gmail.com>
2026-06-01 14:31:09 +0530

    developed the shift form and shift management pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AttendancePage from "./pages/Dashboards/FacultyDashboard/AttendancePage";
 import PermissionPage from "./pages/Dashboards/FacultyDashboard/PermissionPage";
 import RegularaizationListPage from "./pages/Common/RegularaizationListPage";
 import MyTeamPage from "./pages/Dashboards/HOD-Dashboard/myTeamPage";
+import ShiftManagement from "./pages/Dashboards/AdminDashboard/shift/ShiftManagement";
 
 function App() {
   return <>
@@ -20,6 +21,7 @@ function App() {
       <Route path="/profile/:empid" element={<ProfilePage />} />
       <Route path="/dashboard/regularizationList" element={<RegularaizationListPage />} />
       <Route path="/dashboard-faculty/my-Team" element={<MyTeamPage />} />
+      <Route path="/dashboard-admin/shifts" element={<ShiftManagement />} />
     </Routes>
   </>
 }
