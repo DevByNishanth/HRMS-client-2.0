12769b0101d263b024db8cfcbe702a8e151740d8
dharshan-m <dharshanmohan2704@gmail.com>
2026-06-02 13:11:38 +0530

    developed the shifts table and integrated api

--- src/pages/Dashboards/AdminDashboard/shift/ShiftBody.jsx
+++ src/pages/Dashboards/AdminDashboard/shift/ShiftBody.jsx
@@ -1,8 +1,32 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import AddShiftForm from './AddShiftForm';
+import { Pencil,Trash2  } from 'lucide-react';
+import {getShifts} from '../../../../services/getShiftService'
 
 export default function ShiftBody() {
     const [showDrawer, setShowDrawer] = useState(false);
+    const[loading, setLoading] = useState(true);
+    const[shifts, setShifts] = useState([]);
+
+    useEffect(()=>{
+        fetchShifts();
+    },[]);
+
+    const fetchShifts = async () => {
+        try {
+        setLoading(true);
+
+        const response = await getShifts();
+
+        console.log("Shift API Response:", response);
+
+        setShifts(response.data);
+        } catch (error) {
+        console.error("Error fetching shifts:", error);
+        } finally {
+        setLoading(false);
+        }
+    };
     
 
     return (
@@ -19,38 +43,64 @@ export default function ShiftBody() {
                 Add Shift
                 </button>
             </div>
-            <div>
-                <table> 
-                    <thead>
+            <div className="overflow-hidden ">
+                <table className="w-full min-w-[650px] border-collapse text-left"> 
+                    <thead className="bg-[#172c46] text-[12px] uppercase tracking-wide text-[#9aacc7]">
                         <tr>
-                            <th>Shift Name</th>
-                            <th>Start Time</th>
-                            <th>End Time</th>
-                            <th>End Time</th>
-                            <th>Grace Time</th>
-                            <th>Working Hours</th>
-                            <th>Actions</th>
+                            <th className="px-4 py-3 font-semibold">Shift Name</th>
+                            <th className="px-4 py-3 font-semibold">Start Time</th>
+                            <th className="px-4 py-3 font-semibold">End Time</th>
+                            <th className="px-4 py-3 font-semibold">Grace Time</th>
+                            <th className="px-4 py-3 font-semibold">Working Hours</th>
+                            <th className="px-4 py-3 font-semibold">Actions</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td>Morning Shift</td>
-                            <td>9:00 AM</td>
-                            <td>5:00 PM</td>
-                            <td>5:30 PM</td>
-                            <td>30 minutes</td>
-                            <td>8 hours</td>
-                            <td>
-                                <button>Edit</button>
-                                <button>Delete</button>
-                            </td>
-                        </tr>
+                    <tbody className="text-[12px] text-[#cad7eb]">
+                        {loading ? (
+                            <tr>
+                                <td colSpan="6" className="text-center py-4">
+                                Loading...
+                                </td>
+                            </tr>
+                            ) : shifts.length === 0 ? (
+                            <tr>
+                                <td colSpan="6" className="text-center py-4">
+                                No Shifts Found
+                                </td>
+                            </tr>
+                            ) : (
+                            shifts.map((shift) => (
+                                <tr
+                                key={shift._id}
+                                className="border-b border-[#132944] last:border-0"
+                                >
+                                    <td className="px-4 py-4">{shift.shiftName}</td>
+                                    <td className="px-4 py-4">{shift.startTime}</td>
+                                    <td className="px-4 py-4">{shift.endTime}</td>
+                                    <td className="px-4 py-4">{shift.graceTime} minutes</td>
+                                    <td className="px-4 py-4">{shift.workingHours} hours</td>
+                                    <td className="px-4 py-4 text-center text-[#8ca1bd]">
+                                        <button
+                                        type="button"
+                                        className="inline-flex h-8 w-8 items-center justify-center rounded-lg bg-[#c4c6d010] transition hover:bg-[#183052] hover:text-white ml-8"
+                                        >
+                                        <Pencil className="h-4 w-4" />
+                                        </button>
+
+                                        <button
+                                        type="button"
+                                        className="inline-flex h-8 w-8 items-center justify-center rounded-lg bg-[#c4c6d010] transition hover:bg-[#183052] hover:text-white ml-2"
+                                        >
+                                        <Trash2 className="h-4 w-4" />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
+                            )}
                     </tbody>
                 </table>
                 {showDrawer && (
-                    <AddShiftForm
-                    onClose={() => setShowDrawer(false)}
-                    />
+                    <AddShiftForm onClose={() => setShowDrawer(false)} />
                 )}
             </div>
         </div>
